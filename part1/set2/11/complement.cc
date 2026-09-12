066557b778ccc8832efa558e86e43c431d500073
DavidDijkman <daviddijkman@gmail.com>
2026-09-12 15:01:07 +0200

    set2 exercises from thursday

--- part1/set2/11/complement.cc
+++ part1/set2/11/complement.cc
@@ -6,10 +6,27 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     int number = stoi(argv[1]);
+    int mask = 1;
 
-    string summation;
     string bits;
+    string sum;
 
-    
+    while (mask != 0)
+    {
+        if (number == (number | mask)) {
+            number -= mask;
+            sum.insert(0, to_string(mask));
+            if (number != 0) {
+                sum.insert(0, " + ");
+            }
+            bits.insert(0, "1");
+        } 
+        else
+        {
+            bits.insert(0, "0");
+        }
+        mask = mask << 1;
+    }
 
+    cout << argv[1] << " = " << bits << " = " << sum << endl;
 }
