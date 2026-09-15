65a441475587bf3844d525d6e1a3834a7c371f81
DavidDijkman <daviddijkman@gmail.com>
2026-09-15 23:00:09 +0200

    finished ex16 and added some explanations to ex13

--- part1/set2/16/rotate.cc
+++ part1/set2/16/rotate.cc
@@ -7,28 +7,44 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    if (argc != 3)
-    {
-        std::cerr << "Usage: rotate <binary value> <rol|ror>\n";
-        return 1;
-    }
 
-    string bits = argv[1];
     string direction = argv[2];
+    string bits = argv[1];
 
     size_t width = bits.length();
-    size_t value = bitset<64>(bits).to_ullong();
-    size_t mask = (size_t{1} << width) - 1;
+    size_t value = 0;
 
+        // we manually construct the value from the bitstring
+    for (size_t bitPos = 0; bitPos < width; bitPos++)
+    {
+        if (bits[bitPos] == '1')
+            value++;
+
+        value *= 2;
+    }
+        // the mask ensures we erase shifted bits outside our width
+    size_t mask = (1 << width) - 1;
+
+        // we use bitwise or to ensure we keep the carry-over when rotating
     size_t newValue = direction == "rol" ?
         (value << 1 | value >> (width - 1)) & mask :
         (value >> 1 | value << (width - 1)) & mask;
 
-    string binaryString =
-        bitset<64>(newValue).to_string().substr(64 - width);
 
-    cout << binaryString << ' ' << newValue << ' '
-               << hex << newValue << '\n';
+        // I want to use bitset here but I don't think that's allowed
+    string newBits = argv[1];
+    if (direction == "rol") 
+    {
+        newBits.insert(width, 1, newBits[0]);
+        newBits.erase(0, 1);
+    }
+    else
+    {
+        newBits.insert(0, 1, newBits[width - 1]);
+        newBits.erase(width, 1);
+    }
+
+    cout << newBits << ' ' << newValue << ' ' << hex << newValue << '\n';
 
     return 0;
 }
