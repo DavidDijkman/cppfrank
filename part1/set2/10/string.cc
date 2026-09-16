a2331f7e2efcad2a783d8ca85a6ad68604aaf319
DavidDijkman <daviddijkman@gmail.com>
2026-09-16 10:25:29 +0200

    final final changes for set2

--- part1/set2/10/string.cc
+++ part1/set2/10/string.cc
@@ -9,6 +9,8 @@ int main()
     getline(cin, text);
 
                 //substr prints from the first, and until last, non-blank
-    cout << "`" << text.substr(text.find_first_not_of(" \t"), 
-        text.find_last_not_of(" \t") - text.find_first_not_of(" \t") + 1) << "\"\n";
+    cout << "`" << text.substr(
+        text.find_first_not_of(" \t"), 
+        text.find_last_not_of(" \t") - text.find_first_not_of(" \t") + 1)
+        << "\"\n";
 }
