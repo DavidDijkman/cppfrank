8ad23b479dbdd4090923a033374d40afb3a85fdd
ChristianB38 <christian_berg2@proton.me>
2026-09-26 12:34:26 +0200

    hand in bug fix

--- part1/set3/24/fun.cc
+++ part1/set3/24/fun.cc
@@ -5,4 +5,5 @@ void fun(string str)
     size_t sum = 0;
     for (size_t idx = 0; idx < str.length(); ++idx)
         sum += str[idx];
+    cout << sum << '\n';
 }
--- part1/set3/24/fun2.cc
+++ part1/set3/24/fun2.cc
@@ -5,4 +5,5 @@ void fun2(string const &str)
     size_t sum = 0;
     for (size_t idx = 0; idx < str.length(); ++idx)
         sum += str[idx];
+    cout << sum << '\n';
 }
